f57f4fe9da82ebdcd02c887d6ab9329cfa77c53d
Chris Magnacca <chrismagnacca@gmail.com>
2013-07-22 20:04:29 -0400

    adding in coveralls.io

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,10 @@ require "rspec"
 require "fakeweb"
 require "net/http"
 require "simplecov"
+require "coveralls"
 
+# coveralls.io, code coverage, & statistics
+Coveralls.wear!
 SimpleCov.start
 
 require File.expand_path(File.dirname(__FILE__) + "/../lib/newegg")
